e2747f9c8d60922408ccbe0d09b9b0982f8e4b96
Michael <mvasilyev04@gmail.com>
2024-11-06 22:54:56 +0300

    interface for password generator

---
+++ includes/generator.h
@@ -0,0 +1,19 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+
+/*
+    Список флагов для generate:
+    --length <number> || -l <numbers> обязательный
+    --symbols || -s
+    --numbers  || -n
+    --uppercase || --lowercase (-uc || -lc)
+    --help || -h
+    Если 0 флагов кроме длины -> случайный пароль 
+*/
+
+int getLength(char* length);
+
+char* generate(int length, int amount_of_params, ...);
+
+#endif // GENERATOR_H
