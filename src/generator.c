40e28bcb9bc5ca644455ac9cb2a38d9379f0252e
Michael <mvasilyev04@gmail.com>
2024-11-06 23:18:47 +0300

    add generates of password

--- src/generator.c
+++ src/generator.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getLength(char* length) {
     return atoi(length);
@@ -15,5 +16,10 @@ char* generate(int length, int amount_of_params, ...) {
     for(int i = 0; amount_of_params; ++i) {
         params[i] = va_arg(flags, char*);
     }
-    
+    char* password = (char*)malloc(length + 1 * sizeof(char));
+    char randomSymbol = rand() % 126 - 33;
+    for(int i = 0; i < length; ++i) {
+        password += randomSymbol;
+    }
+    return password;
 }
