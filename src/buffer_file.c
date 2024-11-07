b98dca357ede4cfb6fbf700cb1383908acc180f7
Michael <mvasilyev04@gmail.com>
2024-11-08 02:57:33 +0300

    did implementetion of interface buffer_file.h

---
+++ src/buffer_file.c
@@ -0,0 +1,21 @@
+#include "../includes/buffer_file.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void save_password_to_buffer_file(char* password, FILE* buffer, const char* filename) {
+    buffer = fopen(filename, "a+");
+    if(buffer == NULL) {
+        perror("can not open file: No such file or directory\n");
+        exit(0);
+    }
+    int len = strlen(password);
+    for(int i = 0; i < len; ++i) {
+        fputs(password[i], buffer);
+    }
+    fclose(buffer);
+}
+
+int remove_buffer_file(const char* filename) {
+    return remove(filename);
+}
