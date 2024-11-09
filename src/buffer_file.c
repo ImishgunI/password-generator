beff21fb05bf91fbfd132f8b773edd123a383214
Michael <mvasilyev04@gmail.com>
2024-11-09 04:51:04 +0300

    add implementation read

--- src/buffer_file.c
+++ src/buffer_file.c
@@ -20,3 +20,19 @@ void save_password_to_buffer_file(char* password, FILE* buffer, const char* file
 int remove_buffer_file(const char* filename) {
     return remove(filename);
 }
+
+char* read_from_file(int length, char* password, const char* filename) {
+    password = malloc(length + 1 * sizeof(char));
+    memset(password, 0, length + 1);
+    FILE* file = fopen(filename, "r");
+    char c = 0;
+    int i = 0;
+    while(!feof(file) && i < length) {
+        c = fgetc(file);
+        password[i] = c;
+        ++i;
+    }
+    password[i + 1] = '\0';
+    fclose(file);
+    return password;
+}
