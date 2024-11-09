154e43b5ed04094152548f86fee2be3f46905b89
Michael <mvasilyev04@gmail.com>
2024-11-09 05:16:00 +0300

    created tests for funcs of save command

---
+++ tests/save_command_test.c
@@ -0,0 +1,21 @@
+#include "../includes/save.h"
+#include "../includes/generator.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void save_to_file_test() {
+    int length = 10;
+    char* password = NULL;
+    password = generate(length, password, 3, "-s", "-n", "-uc");
+    FILE* acc_file = NULL;
+    const char* filename = "../account.txt";
+    char* account = "mishgun";
+    save_to_file(acc_file, filename, password, account);
+    list(filename);
+}
+
+int main() {
+    save_to_file_test();
+}
