31251d49c61e3d5805340e2e194868aae293b89f
mishgun <mvasilyev04@gmail.com>
2024-11-09 15:36:03 +0300

    added help func

--- includes/save.h
+++ includes/save.h
@@ -3,7 +3,9 @@
 #include <stdio.h>
 /*
     --name <account_name>
+    --help | -h
 */
 void save_to_file(FILE* file, const char* filename, char* password, char* account_name);
 void list(const char* filename);
+void s_help();
 #endif 
